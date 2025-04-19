ef0849cdd436bc97e67067cf8aa139c353bc5793
Timothy Lewis <129003345+NamefulEntity@users.noreply.github.com>
2025-04-18 19:49:29 -0500

    Initial Commit

---
+++ Need Tracker/Need_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Need_TrackerApp.swift
+//  Need Tracker
+//
+//  Created by Timothy Lewis on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Need_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
